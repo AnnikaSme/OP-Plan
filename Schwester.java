fc6c0751747dbfa918ba7d3e59c3022aee8d7209
lilli <lillistoffels25@gmail.com>
2020-10-01 15:45:38 +0200

    java.doc

--- Schwester.java
+++ Schwester.java
@@ -1,6 +1,9 @@
 
 /**
- * In dieser Klasse werden die Schwestern mit Name, Personalnummer, Arbeitszweig, Kontakt und Station festgelegt. 
+ * Diese Klasse Schwester ist eine vererbte Klassse der abstrakten Klasse Personal.
+ * Die Klasse definiert die Schwester mit Name, Personalnummer, Arbeitszweig, Kontakt und Station.
+ * Es gibt 4 Stationen: Chirurgie=1, Kinderstation = 2, Herzstation = 3, Innere Medizin = 4.
+ * Der Arbeitszweig ist "Schwester"
  * 
  * @author Lilli Stoffels, Annika Smetaczko
  * @version 01.10.2020, 1.1
@@ -10,7 +13,12 @@ public class Schwester extends Personal
     private int Station; //Station auf der die Schwester arbeitet, Chirurgie=1, Kinderstation = 2, Herzstation = 3, Innere Medizin = 4
 
      /**
-     * Konstruktor für Objekte der Klasse Schwester, die Attribute der abstrakten Klasse werden vererbt. 
+     * Konstruktor für Objekte der Klasse Schwester. 
+     * Die Attribute der abstrakten Klasse sind geerbt. 
+     * @param Station Station der Schwester
+     * @param Name Name der Schwester
+     * @param Personalnummer Personalnummer der Schwester
+     * @param Kontakt Kontakt der Schwester
      */
     public Schwester(int Station, String Name, int Personalnummer, String Kontakt)
     {
@@ -19,7 +27,7 @@ public class Schwester extends Personal
     }
 
     /**
-     * Konstruktor für Objekte der Klasse Schwester, die Attribute der abstrakten Klasse werden vererbt. 
+     * Standardkonstruktor der Klasse Schwester mit ausgedachten Werten. 
      */
     public Schwester()
     {
@@ -28,10 +36,10 @@ public class Schwester extends Personal
     }
     
     /**
-     * Set-Methode für die Station. Es gibt nur 4 Stationen und dementsprechend ist die Eingabe <1 und >4 ungültig.
-     * Auf ungültige Eingabe wird mit Ausgabe auf dem Bildschirm hingewiesen.
+     * Set-Methode für die Station. Es gibt nur 4 Stationen. Diese Methode prüft, dass keine Stationen <1 und >4 eingegeben werden.
+     * Bei ungültiger Eingabe wird mit einer Bildschirmausgabe darauf hingewiesen.
      * 
-     * @param  Station  Es gibt 4 Stationen welche mit Nummern gekennzeichnet sind: Chirurgie=1, Kinderstation=2, Herzstation=3, Innere Medizin=4
+     * @param  Station  Es gibt 4 Stationen, welche mit Nummern gekennzeichnet sind: Chirurgie=1, Kinderstation=2, Herzstation=3, Innere Medizin=4
      */
     public void SetStation(int Station)
     {
@@ -47,7 +55,7 @@ public class Schwester extends Personal
     /**
      * Get-Methode für Station gibt die Informationen 
      * 
-     * @return    Station   
+     * @return    Station   (als Ziffer, die für eine bestimmte Station steht)
      */
     public int GetStation()
     {
