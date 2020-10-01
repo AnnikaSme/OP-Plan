fc0e79e9575d808a767e1df2c3de5954db3496e4
AnnikaSme <annika.smetaczko@hs-furtwangen.de>
2020-10-01 11:59:58 +0200

    version geändert

--- Kardiotechniker.java
+++ Kardiotechniker.java
@@ -2,8 +2,8 @@
 /**
  * Beschreiben Sie hier die Klasse Kardiotechniker.
  * 
- * @author (Lilli Stoffels, Annika Smetaczko) 
- * @version (30.09.2020 1.0)
+ * @author Lilli Stoffels, Annika Smetaczko
+ * @version 01.10.2020, 1.1
  */
 public class Kardiotechniker extends Personal
 {
