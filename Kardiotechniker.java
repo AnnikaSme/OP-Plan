2b8acbb320f47aba6ade48a81cd14235938a4e8b
AnnikaSme <annika.smetaczko@hs-furtwangen.de>
2020-10-01 11:13:26 +0200

    KT zweiter Konstruktor erstellt

--- Kardiotechniker.java
+++ Kardiotechniker.java
@@ -15,4 +15,12 @@ public class Kardiotechniker extends Personal
         super(Name, Personalnummer, "Kardiotechniker", Kontakt);     // Arbeitszweig wird fest auf Kardiotechniker festgelegt. 
          
     }
+    
+     /**
+     * Konstruktor für Objekte der Klasse KT
+     */
+    public Kardiotechniker( )
+    {
+        super("Müller", 3999302, "Kardiotechniker", "müller.Kardiotechniker@Perfecthospital.de");    
+    }
 }
