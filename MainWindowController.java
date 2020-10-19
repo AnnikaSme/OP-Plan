5de231aed1b196eaa0e8001de6627e9667dcdcf9
AnnikaSme <annika.smetaczko@hs-furtwangen.de>
2020-10-19 11:49:28 +0200

    Oberfläche Grundstruktur der Klasse

---
+++ MainWindowController.java
@@ -0,0 +1,58 @@
+
+/**
+ * Die Klasse MainWindowController dient zur Steuerung der Oberfläche.
+ * 
+ * @author Annika Smetaczko 
+ * @version 19.10.2020, 0.0.1
+ */
+
+import javafx.application.*;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.scene.text.*;
+import javafx.collections.*;
+import javafx.fxml.*;
+import java.io.*;
+import javafx.scene.control.Alert;
+import java.awt.Frame;
+import java.util.*;
+import java.util.*;
+import java.io.File;
+import org.apache.poi.xssf.usermodel.*;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.commons.collections4.*;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.*;
+import java.text.*;
+import java.time.format.*;
+import java.time.*;
+public class MainWindowController extends Verwaltung
+{
+     // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    private int x;
+
+    /**
+     * Konstruktor für Objekte der Klasse n
+     */
+    public MainWindowController() throws Exception
+    {
+        
+    }
+
+    /**
+     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
+     * 
+     * @param  y    ein Beispielparameter für eine Methode
+     * @return        die Summe aus x und y
+     */
+    public int beispielMethode(int y)
+    {
+        // tragen Sie hier den Code ein
+        return x + y;
+    }
+}
